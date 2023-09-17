f775c6ba53ae8589ba32133482662de11c6e4dd7
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-09-17 12:36:53 +0500

    Removed extra code from clickablebox

--- src/components/ClickableBox/index.jsx
+++ src/components/ClickableBox/index.jsx
@@ -1,19 +1,8 @@
-import React, { useState, useEffect } from "react";
 import "./index.css";
 
 function ClickableBox({ image, onClick }) {
-  const [animationState, setAnimationState] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setAnimationState((prevState) => (prevState + 1) % 4);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className="box-container" onClick={onClick}>
-      <div className={`border-animation state-${animationState}`}></div>
       <img src={image} alt="next" className="next-image" />
     </div>
   );
