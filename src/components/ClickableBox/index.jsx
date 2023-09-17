e9362ae495822fdbc3c770eaf694e15f173e05e8
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-09-17 13:06:01 +0500

    Added line animation around the next image button

--- src/components/ClickableBox/index.jsx
+++ src/components/ClickableBox/index.jsx
@@ -1,9 +1,11 @@
 import "./index.css";
+import StrokedSquare from "../StrokedSquare";
 
 function ClickableBox({ image, onClick }) {
   return (
     <div className="box-container" onClick={onClick}>
       <img src={image} alt="next" className="next-image" />
+      <StrokedSquare/>
     </div>
   );
 }
