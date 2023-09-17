2f686c0573645e84b5a769cebe54ad0f146b31c4
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-09-17 17:17:07 +0500

    Infinite loop of square

--- src/components/StrokedSquare/index.jsx
+++ src/components/StrokedSquare/index.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
+
 const StrokedSquare = () => {
+    const [key, setKey] = useState(0); // Unique key to remount component and re-trigger animation
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setKey(prevKey => prevKey + 1); // Update the key to force re-render and re-trigger animation
+        }, 4000); // 4 seconds 
+
+        return () => {
+            clearTimeout(timer); // Cleanup timer to avoid any unexpected behavior
+        };
+    }, [key]);
+
     return (
-        <div className="square">
+        <div className="square" key={key}>
             <div className="border-horizontal"></div>
             <div className="border-vertical"></div>
         </div>
