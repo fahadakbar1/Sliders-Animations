3dcc709aa4847e37bf1b00ad4282fca2528aedd7
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-09-17 14:39:32 +0500

    Added second slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Slider from './components/Slider';
+import SecondSlider from './components/SecondSlider';
 import './App.css';
 
 const App = ()=>{
   return (
     <div className="App">
     <Slider />
+    <SecondSlider/>
     </div>
     );
 }
