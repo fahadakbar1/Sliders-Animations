52e1c06e11703f9e6a82b5166fda822ef7f826e1
fahadakbar1 <muhammadfahadakbar1@gmail.com>
2023-09-18 12:55:09 +0500

    Fix the delay of main image

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -73,11 +73,10 @@ function Slider() {
 
   return (
     <div className="slider-container">
-    <img 
-      src={images[(index + 1) % images.length]} 
-      alt="next" 
+    <div
       className="next-image" 
-    />
+      style={{ backgroundImage: `url(${images[(index + 1) % images.length]})` }}
+    ></div>
     <div
       className="image-part top-image" 
       style={{ backgroundImage: `url(${images[index]})` }}
